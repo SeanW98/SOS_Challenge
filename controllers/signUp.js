e6c73049a0dae947e00a6e9e77c1d6a6a834b15e
SeanW98 <46512818+SeanW98@users.noreply.github.com>
2020-07-01 22:46:00 -0400

    Added functionqlity to sign in page and updated user model

--- controllers/signUp.js
+++ controllers/signUp.js
@@ -1,18 +1,40 @@
 const User = require('../models/user');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 
-exports.get_signUp = function(req, res, next) {
+exports.get_signUp = function (req, res, next) {
     res.render('signUp');
 }
 
-exports.addUser = function(req, res, next) {
-
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: req.body.password
-    });
-    user.save();
-    res.redirect('/');
+exports.addUser = function (req, res, next) {
+    if (req.body.password == req.body.passwordCheck) {
+        User.find({email: req.body.email})
+            .exec()
+            .then(user => {
+                if (user.length >= 1) {
+                    res.send("This email already has an account. Please try to sign up again.")
+                } else{bcrypt.hash(req.body.password, 10, function (err, hash)  {
+                    if (err) {
+                        res.send("500 error")
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            firstName: req.body.firstName,
+                            lastName: req.body.lastName,
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user
+                            .save()
+                            .then( function () {
+                                res.redirect('/');
+                            }
+                            );
+                    }
+                })}
+            })
+    } else {
+        return res.send("Passwords did not match. Please try to sign up again.");
+    }
 }
