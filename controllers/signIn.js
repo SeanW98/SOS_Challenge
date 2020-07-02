3ef4bd1bfa8ba1cefa84749ceb8ff038f2d4f537
SeanW98 <46512818+SeanW98@users.noreply.github.com>
2020-07-01 23:45:07 -0400

    Updated signIn page

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -1,6 +1,30 @@
 const mongoose = require('mongoose');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 exports.get_signIn = function(req, res, next) {
     res.render('signIn');
-    res.redirect('/');
 }
+
+exports.signIn = function(req, res, next) {
+        User.find({email: req.body.email})
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                res.send("Error: password or email not correct. Try again")
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) =>{
+                if (err) {
+                    res.send("Error, password or email not correct. Try again")
+                }
+                if (result) {
+                    res.redirect('/');
+                }else{
+                    res.send("Error, password or email not correct. Try again")
+                }
+            });
+        })
+        .catch(err =>{
+            res.respond("500 error")
+        });
+    };
--- routes/signIn.js
+++ routes/signIn.js
@@ -2,41 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 let signIn = require('../controllers/signIn');
-const { json } = require('express');
 
-router.get('/',signIn.get_signIn);
-
-router.post('/login', (req, res, next) => {
-    User.find({email: req.body.email})
-    .exec()
-    .then(user => {
-        if (user.length < 1) {
-            return res.status(401).json({
-                message: 'Auth failed'
-            });
-        }
-        bcrypt.compare(req.body.password, user[0].password, (err, result) =>{
-            if (err) {
-                return res.status(401).json({
-                    message: 'Auth failed'
-                })
-            }
-            if (result) {
-                return res.status(200).json({
-                    message: 'Auth successful'
-                });
-            }
-            res.status(401).json({
-                message: 'Auth failed'
-            });
-        });
-    })
-    .catch(err =>{
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.get('/', signIn.get_signIn);
+router.post('/', signIn.signIn);
 
 module.exports = router;
