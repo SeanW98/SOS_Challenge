3ecd507814d58d98ce46f2d86a94605e731933a5
SeanW98 <46512818+SeanW98@users.noreply.github.com>
2020-07-16 13:22:52 -0500

    Foundations for search bar created and is working

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,6 +1,17 @@
+const Business = require('../models/business');
 
 exports.get_landing = function(req, res) {
     res.render('landing');
+};
+
+exports.search = function(req, res) {
+    const name = req.body.name;
+    const location = req.body.location; //location will be used for later development
+    Business.find({businessName: {$regex:name, $options: "^i"}})
+    .exec()
+        .then(search => {
+            res.render('search',{results: search});
+        })
 };
 
 //have 3 diffrent ladning functions. 
--- routes/landing.js
+++ routes/landing.js
@@ -5,5 +5,6 @@ const landing = require('../controllers/landing');
 /* GET home page. */
 
 router.get('/', landing.get_landing);
+router.post('/search', landing.search); 
 
 module.exports = router;
