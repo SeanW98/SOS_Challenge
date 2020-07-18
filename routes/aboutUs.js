f8f630f89398985e8cf741ed40ea90551f5a8263
SeanW98 <46512818+SeanW98@users.noreply.github.com>
2020-07-18 15:07:13 -0500

    Fixed router error

--- routes/aboutUs.js
+++ routes/aboutUs.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const aboutUs = require('../controllers/aboutUs');
 
 router.get('/', aboutUs.get_aboutUs);
-router.post('/', aboutUs.aboutUs);
 
 module.exports = router;
